ac0ebc449f87658b8708774af9fc0c327d03e461
Scott Kingsley Clark <lol@scottkclark.com>
2018-06-18 19:30:42 -0500

    Update skc-post-locking.js

--- js/skc-post-locking.js
+++ js/skc-post-locking.js
@@ -6,6 +6,47 @@
 	// Check every 15 seconds for post lock changes.
 	var heartbeat_interval = 15;
 
+	/**
+	 * Handle updating the notice.
+	 *
+	 * @param lock_error String
+	 * @param wrap       Object
+	 */
+	function update_notice( lock_error, wrap ) {
+		if ( ! wrap.length ) {
+			return;
+		}
+
+		// Show wrap if it is hidden.
+		if ( wrap.hasClass( 'hidden' ) ) {
+			wrap.removeClass( 'hidden' );
+		}
+
+		// Set user fields.
+		$( '.skc-post-locking-user-display-name', wrap ).text( received.lock_error.display_name );
+		$( '.skc-post-locking-user-role', wrap ).text( received.lock_error.role );
+	}
+
+	/**
+	 * Handle removing the notice.
+	 *
+	 * @param wrap Object
+	 */
+	function remove_notice( wrap ) {
+		if ( ! wrap.length ) {
+			return;
+		}
+
+		// Hide wrap if it is not hidden.
+		if ( ! wrap.hasClass( 'hidden' ) ) {
+			wrap.addClass( 'hidden' );
+		}
+
+		// Clear user fields.
+		$( '.skc-post-locking-user-display-name', wrap ).text( '' );
+		$( '.skc-post-locking-user-role', wrap ).text( '' );
+	}
+
 	$( document ).on( 'heartbeat-send.refresh-lock', function ( e, data ) {
 		/**
 		 * Heartbeat locks.
@@ -15,7 +56,9 @@
 		 * When the user does not send a heartbeat in a heartbeat-time
 		 * the user is no longer editing and another user can start editing.
 		 */
-		var lock = $( '#skc_post_locking_active' ).val(), post_id = $( '#skc_post_locking_post_id' ).val(), send = {};
+		var lock = $( '#skc_post_locking_active' ).val(),
+			post_id = $( '#skc_post_locking_post_id' ).val(),
+			send = {};
 
 		if ( !post_id || !$( '#skc_post_locking_dialog' ).length ) {
 			return;
@@ -28,44 +71,96 @@
 		}
 
 		data['wp-refresh-post-lock'] = send;
-
 	} ).on( 'heartbeat-tick.refresh-lock', function ( e, data ) {
 		// Post locks: update the lock string or show the dialog if somebody has taken over editing.
-		var received, wrap;
+		var received = {},
+			wrap = {};
 
 		if ( data['wp-refresh-post-lock'] ) {
 			received = data['wp-refresh-post-lock'];
 
+			wrap = $( '#skc_post_locking_dialog' );
+
 			if ( received.lock_error ) {
-				// Show "editing taken over" message.
-				wrap = $( '#skc_post_locking_dialog' );
+				// Update the notice.
+				update_notice( received.lock_error, wrap );
 
-				if ( wrap.length ) {
-					// Show wrap if it is hidden.
-					if ( wrap.hasClass( 'hidden' ) ) {
-						wrap.removeClass( 'hidden' );
-					}
+				// Hide ACF form on page.
+				$( '.acf-form' ).addClass( 'hidden skc-post-locking-form-inactive' );
 
-					// Set user fields.
-					$( '.skc-post-locking-user-display-name', wrap ).text( received.lock_error.display_name );
-					$( '.skc-post-locking-user-role', wrap ).text( received.lock_error.role );
+				// Hide Pods form on page.
+				$( '.pods-form' ).addClass( 'hidden skc-post-locking-form-inactive' );
 
-					// Hide ACF form on page.
-					$( '.acf-form' ).addClass( 'hidden' );
-
-					// Hide Pods form on page.
-					$( '.pods-form' ).addClass( 'hidden' );
-
-					// Hide GF form on page.
-					$( '.gform_wrapper' ).addClass( 'hidden' );
-				}
+				// Hide GF form on page.
+				$( '.gform_wrapper' ).addClass( 'hidden skc-post-locking-form-inactive' );
 			}
 			else {
 				if ( received.new_lock ) {
 					// Save current active lock.
 					$( '#skc_post_locking_active' ).val( received.new_lock );
 				}
+
+				// Remove the notice
+				remove_notice( wrap );
+
+				// Show ACF form on page.
+				$( '.acf-form.skc-post-locking-form-inactive' ).removeClass( 'hidden skc-post-locking-form-inactive' );
+
+				// Show Pods form on page.
+				$( '.pods-form.skc-post-locking-form-inactive' ).removeClass( 'hidden skc-post-locking-form-inactive' );
+
+				// Show GF form on page.
+				$( '.gform_wrapper.skc-post-locking-form-inactive' ).removeClass( 'hidden skc-post-locking-form-inactive' );
 			}
+		}
+	} ).on( 'heartbeat-tick.wp-check-locked-posts', function ( e, data ) {
+		var locked = data['wp-check-locked-posts'] || {};
+
+		$( '.skc-post-locking-row' ).each( function ( i, el ) {
+			var row = $( el ),
+				post_id = row.data( 'post-id' ),
+				key = 'post-' + post_id,
+				lock_error = {};
+
+			if ( ! post_id ) {
+				return;
+			}
+
+			wrap = $( '.skc-post-locking-list-dialog', row );
+
+			if ( locked.hasOwnProperty( key ) ) {
+				lock_error = locked[key];
+
+				// Update the notice.
+				update_notice( received.lock_error, wrap );
+
+				// Mark row as locked.
+				row.addClass( 'wp-locked' );
+			} else {
+				// Remove the notice.
+				remove_notice( wrap );
+
+				// Mark row as not locked.
+				if ( row.hasClass( 'wp-locked' ) ) {
+					row.removeClass( 'wp-locked' );
+				}
+			}
+		} );
+	} ).on( 'heartbeat-send.wp-check-locked-posts', function ( e, data ) {
+		var check = [];
+
+		$( '.skc-post-locking-row' ).each( function ( i, el ) {
+			var row = $( el ),
+				post_id = row.data( 'post-id' ),
+				key = 'post-' + post_id;
+
+			if ( post_id ) {
+				check.push( key );
+			}
+		} );
+
+		if ( check.length ) {
+			data['wp-check-locked-posts'] = check;
 		}
 	} ).ready( function () {
 		// Set the heartbeat interval.
