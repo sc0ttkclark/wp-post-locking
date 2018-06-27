9440638223f7a989c06c57d4df7a5dddc3f724bb
Scott Kingsley Clark <lol@scottkclark.com>
2018-06-26 19:04:25 -0500

    Update skc-post-locking.js

--- js/skc-post-locking.js
+++ js/skc-post-locking.js
@@ -13,7 +13,7 @@
 	 * @param wrap       Object
 	 */
 	function update_notice( lock_error, wrap ) {
-		if ( ! wrap.length ) {
+		if ( !wrap.length ) {
 			return;
 		}
 
@@ -33,12 +33,12 @@
 	 * @param wrap Object
 	 */
 	function remove_notice( wrap ) {
-		if ( ! wrap.length ) {
+		if ( !wrap.length ) {
 			return;
 		}
 
 		// Hide wrap if it is not hidden.
-		if ( ! wrap.hasClass( 'hidden' ) ) {
+		if ( !wrap.hasClass( 'hidden' ) ) {
 			wrap.addClass( 'hidden' );
 		}
 
@@ -56,9 +56,7 @@
 		 * When the user does not send a heartbeat in a heartbeat-time
 		 * the user is no longer editing and another user can start editing.
 		 */
-		var lock = $( '#skc_post_locking_active' ).val(),
-			post_id = $( '#skc_post_locking_post_id' ).val(),
-			send = {};
+		var lock = $( '#skc_post_locking_active' ).val(), post_id = $( '#skc_post_locking_post_id' ).val(), send = {};
 
 		if ( !post_id || !$( '#skc_post_locking_dialog' ).length ) {
 			return;
@@ -73,9 +71,7 @@
 		data['wp-refresh-post-lock'] = send;
 	} ).on( 'heartbeat-tick.refresh-lock', function ( e, data ) {
 		// Post locks: update the lock string or show the dialog if somebody has taken over editing.
-		var received = {},
-			wrap = {},
-			lock_error = {};
+		var received = {}, wrap = {}, lock_error = {};
 
 		if ( data['wp-refresh-post-lock'] ) {
 			received = data['wp-refresh-post-lock'];
@@ -98,8 +94,7 @@
 				$( '.gform_wrapper' ).addClass( 'hidden skc-post-locking-form-inactive' );
 
 				$( document ).trigger( 'skc-post-locking-single-locked', {
-					wrap: wrap,
-					lock_error: received.lock_error
+					wrap : wrap, lock_error : received.lock_error
 				} );
 			}
 			else {
@@ -122,7 +117,7 @@
 
 				// Trigger event.
 				$( document ).trigger( 'skc-post-locking-single-unlocked', {
-					wrap: wrap
+					wrap : wrap
 				} );
 			}
 		}
@@ -130,13 +125,10 @@
 		var locked = data['wp-check-locked-posts'] || {};
 
 		$( '.skc-post-locking-list-dialog' ).each( function ( i, el ) {
-			var row = $( el ),
-				post_id = row.data( 'post-id' ),
-				key = 'post-' + post_id,
-				parent_wrapper = row.parent(),
+			var row = $( el ), post_id = row.data( 'post-id' ), key = 'post-' + post_id, parent_wrapper = row.parent(),
 				lock_error = {};
 
-			if ( ! post_id ) {
+			if ( !post_id ) {
 				return;
 			}
 
@@ -147,17 +139,16 @@
 				update_notice( lock_error, row );
 
 				// Mark row as locked.
-				if ( ! parent_wrapper.hasClass( 'wp-locked' ) ) {
+				if ( !parent_wrapper.hasClass( 'wp-locked' ) ) {
 					parent_wrapper.addClass( 'wp-locked' );
 				}
 
 				// Trigger event.
 				$( document ).trigger( 'skc-post-locking-list-locked', {
-					lock_error: lock_error,
-					row: row,
-					parent: parent_wrapper
+					lock_error : lock_error, row : row, parent : parent_wrapper
 				} );
-			} else {
+			}
+			else {
 				// Remove the notice.
 				remove_notice( row );
 
@@ -168,8 +159,7 @@
 
 				// Trigger event.
 				$( document ).trigger( 'skc-post-locking-list-unlocked', {
-					row: row,
-					parent: parent_wrapper
+					row : row, parent : parent_wrapper
 				} );
 			}
 		} );
@@ -177,9 +167,7 @@
 		var check = [];
 
 		$( '.skc-post-locking-list-dialog' ).each( function ( i, el ) {
-			var row = $( el ),
-				post_id = row.data( 'post-id' ),
-				key = 'post-' + post_id;
+			var row = $( el ), post_id = row.data( 'post-id' ), key = 'post-' + post_id;
 
 			if ( post_id ) {
 				check.push( key );
@@ -194,5 +182,51 @@
 		if ( typeof wp !== 'undefined' && wp.heartbeat ) {
 			wp.heartbeat.interval( heartbeat_interval );
 		}
+	} );
+
+	/*
+	 * When the user is trying to load another page, or reloads current page
+	 * show a confirmation dialog when there are unsaved changes.
+	 */
+	$( window ).on( 'unload.edit-post', function ( event ) {
+		/*
+		 * Unload is triggered (by hand) on removing the Thickbox iframe.
+		 * Make sure we process only the main document unload.
+		 */
+		if ( event.target && event.target.nodeName != '#document' ) {
+			return;
+		}
+
+		var postID = $( '#skc_post_locking_post_id' ).val();
+		var postLock = $( '#skc_post_locking_active' ).val();
+
+		if ( !postID || !postLock ) {
+			return;
+		}
+
+		var data = {
+			action           : 'wp-remove-post-lock',
+			_wpnonce         : $( '#skc_post_locking_wpnonce' ).val(),
+			post_ID          : postID,
+			active_post_lock : postLock
+		};
+
+		if ( window.FormData && window.navigator.sendBeacon ) {
+			var formData = new window.FormData();
+
+			$.each( data, function ( key, value ) {
+				formData.append( key, value );
+			} );
+
+			if ( window.navigator.sendBeacon( ajaxurl, formData ) ) {
+				return;
+			}
+		}
+
+		// Fall back to a synchronous POST request.
+		// See https://developer.mozilla.org/en-US/docs/Web/API/Navigator/sendBeacon
+		$.post( {
+			async : false, data : data, url : ajaxurl
+		} );
 	} );
 }( jQuery ));
